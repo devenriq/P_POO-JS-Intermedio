00e105c81ecf9b94c43a71bd489172ce9bd2b5e7
Enrique <octepc.97@gmail.com>
2022-04-03 18:27:01 -0500

    Aplicados más métodos como getOwnPropertyDescriptors para modificar los valores del objeto y ver qué pasaba

--- index.js
+++ index.js
@@ -11,13 +11,41 @@ const juan = {
 // console.log(Object.getOwnPropertyNames(juan));
 // console.log(Object.entries(juan));
 
-console.log(Object.getOwnPropertyDescriptors(juan));
+// console.log(
+//   Object.defineProperty(juan, "pruebaNASA", {
+//     value: "extraterrestres",
+//     enumerable: true,
+//     writable: true,
+//     configurable: true,
+//   })
+// );
 
-console.log(
-  Object.defineProperty(juan, "pruebaNASA", {
-    value: "extraterrestres",
-    enumerable: true,
-    writable: true,
-    configurable: true,
-  })
-);
+Object.defineProperty(juan, "navigator", {
+  value: "Chrome",
+  writable: true,
+  enumerable: false,
+  configurable: true,
+});
+
+Object.defineProperty(juan, "editor", {
+  value: "VSCode",
+  writable: false,
+  enumerable: true,
+  configurable: true,
+});
+
+Object.defineProperty(juan, "terminal", {
+  value: "WSL",
+  writable: true,
+  enumerable: true,
+  configurable: false,
+});
+
+Object.defineProperty(juan, "pruebaNasa", {
+  value: "Extraterrestres",
+  writable: false,
+  enumerable: false,
+  configurable: false,
+});
+
+// console.log(Object.getOwnPropertyDescriptors(juan));
